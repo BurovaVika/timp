e1d73041fa55c6c6e633fa0770ec73b9d15f12a1
BurovaVika <47822711+BurovaVika@users.noreply.github.com>
2019-03-02 15:25:07 +0700

    невозрастающая поседовательность через цикл for

--- 11_vikaRED.c
+++ 11_vikaRED.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 int main(void)
 {
-int n = 0,a=0,b = 0;
-scanf("%d", &n);
-scanf("%d",&b);
-for (int i=1;i<n;i++)
-{
-if ((i%2)==1)  
-{
-scanf("%d",&a); if (b>=a) printf(" 1\n"); else {printf(" 0\n");
-}}
-if ((i%2)==0){scanf("%d",&b); if(a>=b) printf("1\n"); else printf (" 0\n");}
+    int n = 0,a = 0,b = 0,rez =-2,c = 0;
+    scanf("%d", &n);
+    scanf("%d", &b);
+    for (int i=1;i<n;i++)
+        {
+scanf("%d",&c);
+        if ((i%2)==1 && rez != 0)  
+             {
+             a = c; if (b>=a) rez = 1; else rez = 0;
+             }
+        if ((i%2)==0 && rez != 0){b=c; if(a>=b) rez = 1; else rez=0 ; 
+             }
 
 }
+printf("%d\n",rez);
 }
+
